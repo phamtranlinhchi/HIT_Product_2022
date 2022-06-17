2642be7ffac30e57267ca41eaebe2473916041d9
vandungday <dungtn492@gmail.com>
2022-06-17 13:20:46 +0700

    login

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,6 +4,7 @@ const asyncHandle = require("../middlewares/asyncHandle");
 const { userService } = require("../services");
 const { User } = require("../models");
 
+<<<<<<< HEAD
 // <<<<<<< HEAD
 const createUser = asyncHandle(async(req, res, next) => {
     const user = await userService.createUser(req.body);
@@ -39,6 +40,43 @@ const getUser = asyncHandle(async(req, res, next) => {
 
 const updateUser = asyncHandle(async(req, res, next) => {
     const user = await userService.updateUserById(req.params.userId, req.body);
+=======
+const createUser = asyncHandle(async(req, res, next) => {
+    const user = await userService.createUser(req.body);
+    res.status(httpStatus.CREATED).send(user);
+});
+
+const getUsers = asyncHandle(async(req, res, next) => {
+    const result = await userService.getUsers(req.query);
+    res.status(httpStatus.OK).json({
+        status: "success",
+        result,
+    });
+});
+
+const getUser = asyncHandle(async(req, res, next) => {
+    const user = await userService.getUserById(req.params.userId);
+    if (!user) {
+        return next(new ErrorResponse(httpStatus.NOT_FOUND, "User not found"));
+    }
+>>>>>>> 10afc9b (top-3-user)
+    res.status(httpStatus.OK).json({
+        status: "success",
+        user,
+    });
+});
+
+<<<<<<< HEAD
+const deleteUser = asyncHandle(async(req, res, next) => {
+    await userService.deleteUserById(req.params.userId);
+    res.status(httpStatus.NO_CONTENT).json({
+        status: "success",
+    });
+});
+
+=======
+const updateUser = asyncHandle(async(req, res, next) => {
+    const user = await userService.updateUserById(req.params.userId, req.body);
     res.status(httpStatus.OK).json({
         status: "success",
         user,
@@ -52,6 +90,7 @@ const deleteUser = asyncHandle(async(req, res, next) => {
     });
 });
 
+>>>>>>> 10afc9b (top-3-user)
 const getUserTopByStar = asyncHandle(async(req, res, next) => {
     const top = await User.aggregate([{
             $sort: { star: -1 },
