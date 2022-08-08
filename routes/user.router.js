4e91de8155ffa6f8f00ea5f7e741241e845fd6ab
Nguyễn Quý <quykaba@gmail.com>
2022-08-09 00:53:29 +0700

    Confix/Swagger_UI

--- routes/user.router.js
+++ routes/user.router.js
@@ -3,19 +3,80 @@ const router = express.Router();
 
 const { userController } = require("../controllers/index");
 const { authController } = require("../controllers/index");
-
+/** 
+ * @swagger 
+ * /api/users/top-user: 
+ *   get: 
+ *     description: Get top 3 user
+ *     responses:  
+ *       200: 
+ *         description: Success  
+ *   
+ */
 router.route("/top-user").get(userController.getUserTopByStar);
 
 router
     .route("/")
-    // .get(authController.protect, userController.getUsers)
-    .get(userController.getUsers)
-    .post(userController.createUser);
+
+/** 
+ * @swagger 
+ * /api/users: 
+ *   get: 
+ *     description: Get top user
+ *     responses:  
+ *       200: 
+ *         description: Success  
+ *   
+ */
+// .get(authController.protect, userController.getUsers)
+.get(userController.getUsers)
+
+/**
+ * @swagger
+ * /api/users:
+ *  post:
+ *    summary: Creates a new user.
+ *    consumes:
+ *      - application/json
+ *    parameters:
+ *      - in: body
+ *        name: user
+ *        description: The user to create.
+ *        schema:
+ *          type: object
+ *          required:
+ *            - userName
+ *          properties:
+ *            email:
+ *              type: string
+ *            username:
+ *              type: string
+ *            password:
+ *              type: string
+ *    responses:
+ *      201:
+ *        description: Created
+ *        content:
+ *          application/json:
+ *           schema: 
+ *              properties: 
+ *                email:string
+ *                username:string
+ *                 password:string
+ */
+.post(userController.createUser);
+
+router
+    .route('/tokenUser')
+    .get(authController.getUserByToken)
 router
     .route("/:userId")
-    .get(userController.getUser)
+
+.get(userController.getUser)
     .put(userController.updateUser)
     .delete(userController.deleteUser);
 
+module.exports = router;
 
+router.route("/:userId").get(userController.getUser).put(userController.updateUser).delete(userController.deleteUser);
 module.exports = router;
