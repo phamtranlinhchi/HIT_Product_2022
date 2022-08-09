53433028c4cc007f3ce66ea79f9216ecd8e6d3d5
vandungday <dungtn492@gmail.com>
2022-08-10 00:14:43 +0700

    swagger

--- routes/book.router.js
+++ routes/book.router.js
@@ -3,9 +3,31 @@ const router = express.Router();
 
 const { bookController } = require("../controllers/index");
 const { authController } = require("../controllers/index");
-
+/**
+ * @swagger
+ * /api/books/:
+ *   get:
+ *     description: Get allbook and create book
+ *     responses:
+ *       200:
+ *         description: Success
+ *
+ */
 router.route("/").get(bookController.getBooks).post(bookController.createBook);
-
+/**
+ *  @swagger
+ * paths:
+ * api/books/{bookID}:
+ *  get:
+ *     summary: Get a book by ID
+ *     parameters:
+ *       - in: path
+ *        name: bookId
+ *        schema:
+ *           type: Object
+ *        required: true
+ *         description: Numeric ID of the user to get
+ */
 router
     .route("/:bookId")
     .get(bookController.getBook)
